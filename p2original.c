1d4ff37bd9255701afe2f9588e01da980ae8ffca
sanjanaraghupat <96220467+sanjanaraghupathi@users.noreply.github.com>
2022-02-02 04:51:37 +0000

    largest of three

---
+++ p2original.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int cmp(int a,int b,int c)
+{
+  if(a>b)
+  {
+    if(a>c)
+    {
+      return a;
+    }
+   else
+   {
+   return c;
+   }
+  }
+  else
+  {
+    if(b>c)
+    {
+      return b;
+    }
+    else
+    {
+      return c;
+    }
+  }
+}
+int input()
+{
+  int x;
+  scanf("%d",&x);
+  return x;
+}
+int main()
+{
+  int a,b,c,lar;
+  printf("enter three numbers\n");
+  a=input();
+  b=input();
+  c=input();
+  lar=cmp(a,b,c);
+  printf("%d is the the largest of three\n",lar);
+  return 0;
+}
