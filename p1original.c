a340e37c9b9e3449d3060400926d65a23134fa74
sanjanaraghupat <96220467+sanjanaraghupathi@users.noreply.github.com>
2022-02-02 04:26:44 +0000

    sum of two numbers using pass by reference

--- p1original.c
+++ p1original.c
@@ -1,2 +1,24 @@
-
+#include <stdio.h>
+void input(int *a,int *b)
+{
+  printf("Enter first number\n");
+  scanf("%d",a);
+  printf("Enter second number\n");
+  scanf("%d",b);
+}
+void add(int a,int b,int *sum)
+{
+  *sum=a+b;
+}
+void output(int a,int b, int sum)
+{
+ printf("%d+%d=%d\n",a,b,sum);
+}
+int main()
+{
+ int a,b,sum;
+ input(&a,&b);
+ add(a,b,&sum);
+ output(a,b,sum);
+ return 0; 
 }
